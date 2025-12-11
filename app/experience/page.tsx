574e9a72a70f237ffd8cad358470a5c59c0358f6
Francisco Javier González <franciscojgonzalezfernandez@gmail.com>
2025-12-11 13:38:50 +0100

    Experience page better ordenated

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,16 +1,18 @@
-import { Header } from "@/components/header"
-import { Experience } from "@/components/experience"
+import { Header } from "@/components/header";
+import { Experiences } from "@/components/experience";
+import { experiences } from "@/data/experiences.data";
+import { architectures } from "@/data/architectures.data";
 
 export const metadata = {
   title: "Experience | Portfolio",
   description: "Professional experience and career history",
-}
+};
 
 export default function ExperiencePage() {
   return (
     <>
       <Header />
-      <Experience />
+      <Experiences experiences={experiences} />
     </>
-  )
+  );
 }
