e51401713ba19927388d459284a64d34e4c64637
Francisco Javier González <franciscojgonzalezfernandez@gmail.com>
2025-12-13 23:04:21 +0100

    Let's check node_env to concat the base path or not.

--- components/high-order-components/CustomImage.tsx
+++ components/high-order-components/CustomImage.tsx
@@ -1,4 +1,5 @@
 const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH ?? "/my-portfolio";
+const ENV = process.env.NODE_ENV;
 
 interface CustomImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   src: string;
